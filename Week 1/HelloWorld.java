d22a38c20b8cb788a3a6db758ecfbc4ebbed8762
Lauren Coseo <65357160+laurencoseo@users.noreply.github.com>
2021-04-24 20:39:58 -0700

    Create HelloWorld.java

---
+++ Week 1/HelloWorld.java
@@ -0,0 +1,12 @@
+// 4/13/21
+// Seattle Central College
+// CS 142
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Print out Hello World
+		System.out.println("Hello World!");
+	}
+
+}
